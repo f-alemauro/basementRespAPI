b64b8e57530ab81d274b3f05bc0187a100494271
afranchi <afranchi@sowre.it>
2020-04-28 08:58:10 +0200

    Added jquery for adding items and upgating page and for adding item definition and update dropdownlist. Started implementing success/error messages. Still missing validation

--- BasementsRestApi/wwwroot/Scripts/myscripts.js
+++ BasementsRestApi/wwwroot/Scripts/myscripts.js
@@ -4,21 +4,67 @@
         if (confirm("Confirm delete?")) {
             $.ajax({
                 type: "DELETE",
-                url: 'api/items/'+id,
+                url: '/api/items/'+id,
              
                 contentType: "application/json; charset=utf-8",
-                success: function (response) {
+                success: function () {
                     $('table#itemList tr#itemRow'+id).remove();
                 },
-                error: function (response) {
+                error: function () {
                     alert("Error in deleting item");
                 }
             });
         }
 })
 
+$(document).on("change", "#item_Quantity",
+    function ()
+    {
+        var quantity = parseInt($(this).val(), 10);
+        if (quantity) {
+            var itemID = $(this).attr("itemID");
+            $.post(
+                "/api/items/" + itemID + "/" + quantity
+            )
+                .done(
+                    function (data, textStatus, jqXHR)
+                    {
+                        alert(data.message)
+                    })
+                .fail(
+                    function (jqXHR, textStatus, errorThrown)
+                    {
+                        alert("error:" + jqXHR.responseJSON.message);
+                    })
+        }
+    
+    }
+);
+
+
+//$('.addAnotherAction').click(function (e) {
+//    e.preventDefault();
+//    var id = $(this).attr("itemID");
+//        $.ajax({
+//            type: "PUSH",
+//            url: '/api/items/' + id,
+
+//            contentType: "application/json; charset=utf-8",
+//            success: function (response) {
+//                $('table#itemList tr#itemRow' + id).remove();
+//            },
+//            error: function (response) {
+//                alert("Error in deleting item");
+//            }
+//        });
+//})
+
 $("#newItemButton").click(function () {
     $("#newItemForm").toggle();
+});
+
+$("#newItemDefButton").click(function () {
+    $("#itemDefsForm").toggle();
 });
 
 
@@ -29,35 +75,80 @@ $("#saveItemButton").click(function () {
     newItem.AddedOn = $('#AddedOn').val();
     newItem.ExpireOn = $('#ExpireOn').val();
     newItem.ItemDefinitionID = $('#itemDefs').val();
-    newItem.UserID = 1;
+    newItem.UserID = $('#usersDef').val();
     var data = JSON.stringify(newItem);
-   
-
+    
     $.ajax({
-        url: 'api/items',
+        url: '/api/items',
         type: 'POST',
         contentType: 'application/json',
         success: function (response) {
-            alert(response.message);
+            if (response.success) {
+                var addedOnDT = response.data.addedOn.substring(0, 10);
+                var expireOnDT = response.data.expireOn.substring(0, 10);
+                $('div.alert').fadeIn(500).addClass(response.cssClassName).html('<strong>' + response.title + '</strong> ' + response.message).delay(3000).fadeOut(500);
+
+                $('#itemList').append('<tr id="' + response.data.itemID + '">' +
+                    '<td>' + response.data.itemDefinition.description + ',' + response.data.itemDefinition.volume +
+                    response.data.itemDefinition.unitOfMeasurement + '</td>' +
+                    '<td><input class="form-control text-box single-line" id="item_Quantity" itemID="' +
+                    response.data.itemID + '" min="1" name="item.Quantity" type="number" value="' + response.data.quantity + '" /></td>' +
+                    '<td>' + addedOnDT + '</td>' +
+                    '<td>' + expireOnDT + '</td>' +
+                    '<td>' + response.data.user.name + '</td>' +
+                    '<td><a href="/Items/Edit?itemID=' + response.data.itemID + '">Edit</a> |' +
+                    '<a class="deleteAction" href="" itemID="' + response.data.itemID + '">Delete</a> </td></tr >');
+            }
+            else {
+                $('div.alert').fadeIn(500).addClass(response.cssClassName).html('<strong>' + response.title + '</strong> ' + response.message).delay(3000).fadeOut(500);
+            }
         },
         fail: function (response) {
-            alert(response.message);
+           
         },
-        data: data
-
-    });
-})  
-
-$(document).ready(function () {
-    $.get("api/itemDefinitions", function (response) {
-        if (response.success) {
-            for (var i = 0; i <= response.data.length; i++) {
-                $('#itemDefs').append('<option value="' + response.data[i].itemDefinitionID + '">' + response.data[i].description + '-' + response.data[i].volume + ' ' + response.data[i].unitOfMeasurement + '</option>');
-            }
+        data: data,
+        complete: function () {
+            $('html, body').animate({ scrollTop: 0 }, 'fast');
+            $("#newItemForm").toggle();
+           
         }
-        
+    });
+    
+})
+
+
+
+$("#addItemDef").click(function () {
+
+    var newItemDef = {};
+    newItemDef.BarCode = $('#Barcode').val();
+    newItemDef.Type = $('#itemDefType').val();
+    newItemDef.Description = $('#Description').val();
+    newItemDef.Volume = $('#Volume').val();
+    newItemDef.UnitOfMeasurement = $('#itemDefUOM').val();
+    var data = JSON.stringify(newItemDef);
+
+    $.ajax({
+        url: '/api/itemDefinitions',
+        type: 'POST',
+        contentType: 'application/json',
+        success: function (response) {
+            if (response.success) {
+                $('div.alert').fadeIn(500).addClass(response.cssClassName).html('<strong>' + response.title + '</strong> ' + response.message).delay(3000).fadeOut(500);
+
+                $('#itemDefs').append('<option value="' + response.data.itemDefinitionID + '" selected>' + response.data.description + '-' + response.data.volume + ' ' + response.data.unitOfMeasurement + '</option>');
+            }
+            else {
+                $('div.alert').fadeIn(500).addClass(response.cssClassName).html('<strong>' + response.title + '</strong> ' + response.message).delay(3000).fadeOut(500);
+            }
+        },
+        fail: function (response) {
+
+        },
+        data: data,
+        complete: function () {
+            $("#itemDefsForm").toggle();
+        }
     });
 
-    
-
-});
+})
