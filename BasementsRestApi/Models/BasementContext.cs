2cefd3ea11bdec67025d2f4fd60a5f13dff94197
afranchi <afranchi@sowre.it>
2020-05-02 23:23:50 +0200

    Minor changes

---
+++ BasementsRestApi/Models/BasementContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace BasementsRestApi.Models
+{
+    public class BasementContext : DbContext
+    {
+        public DbSet<Item> Items { get; set; }
+        public DbSet<ItemDefinition> ItemDefinitions { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //optionsBuilder.UseSqlite(@"Filename=DB\Basement.db", options =>
+            //{
+            //    options.MigrationsAssembly(Assembly.GetExecutingAssembly().FullName);
+            //});
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        public BasementContext()
+       : base()
+        {
+        }
+
+    }
+}
---
+++ BasementsRestApi/ViewModels/UserMessageVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BasementsRestApi.ViewModels
+{
+    public class UserMessageVM
+    { 
+        public string CssClassName { get; set; }
+        public string Title { get; set; }
+        public string Message { get; set; }
+    }
+}
